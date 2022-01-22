1fdcd40cd5cc07ba9e87de3687fa1220ab42a32d
Iaroslav Bondarenko <iabondar@cisco.com>
2022-01-23 01:47:59 +0300

    Migrated to uWSGI

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ import jinja2
 import common
 import threading
 from flask import Flask, redirect, request, url_for
+from flask_caching import Cache
 
 app = Flask(__name__)
-
-set_length = 0
+cache = Cache()
+cache.init_app(app=app, config={"CACHE_TYPE": "filesystem",'CACHE_DIR': '/tmp'})
+cache.set("set_length", 0)
 
 
 def rediscover(name: str, access_type: str, ip: str, username: str, passwd: str):
-    global set_length
-
     node = common.Node.create_node(common.AccessType(access_type), name)
     node.id = 1
     node.ipaddr.add(ip)
@@ -24,12 +24,12 @@ def rediscover(name: str, access_type: str, ip: str, username: str, passwd: str)
     newset = node.parse_neighbors(nodeset, edgeset)
     while len(newset) != 0:
         print(f"Nodes left to query: {len(newset)}")
-        set_length = len(newset)
+        cache.set("set_length", len(newset))
         newnode = newset.pop()
         tmpset = newnode.parse_neighbors(nodeset, edgeset)
         newset = newset.union(tmpset)
 
-    set_length = 0
+    cache.set("set_length", 0)
     templateLoader = jinja2.FileSystemLoader(searchpath="./jinja2")
     templateEnv = jinja2.Environment(loader=templateLoader)
     TEMPLATE_FILE = "index.j2"
@@ -42,12 +42,11 @@ def rediscover(name: str, access_type: str, ip: str, username: str, passwd: str)
 
 @app.route("/rediscover", methods=['POST'])
 def front_rediscover():
-    global set_length
     with open("static/index.html", "w") as f:
         f.write("Rediscovering")
 
-    if set_length == 0:
-        set_length = 1
+    if cache.get("set_length") == 0:
+        cache.set("set_length", 1)
         name = request.form["name"]
         ip = request.form["ip"]
         username = request.form["login"]
@@ -60,18 +59,15 @@ def front_rediscover():
 
 @app.route("/status", methods=['GET'])
 def front_status():
+    set_length = cache.get("set_length")
     return f'<meta http-equiv="refresh" content="5">Nodes left to query: {set_length}'
 
 
 @app.route("/status_rediscover", methods=['GET'])
 def front_status_rediscover():
-    global set_length
+    set_length = cache.get("set_length")
 
     if set_length == 0:
         return redirect('/')
 
     return f'<meta http-equiv="refresh" content="5">Nodes left to query: {set_length}'
-
-
-if __name__ == '__main__':
-    app.run(host='localhost', port=5000)
