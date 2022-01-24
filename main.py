9997e40614705539120f1b4ffeff6c74b70f2068
Iaroslav Bondarenko <iabondar@cisco.com>
2022-01-24 14:49:25 +0300

    Set cache timeout to infinite - otherwise it becomes None after a while

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from flask_caching import Cache
 
 app = Flask(__name__)
 cache = Cache()
-cache.init_app(app=app, config={"CACHE_TYPE": "filesystem",'CACHE_DIR': '/tmp'})
+cache.init_app(app=app, config={"CACHE_TYPE": "filesystem",'CACHE_DIR': '/tmp', "CACHE_DEFAULT_TIMEOUT": 0})
 cache.set("set_length", 0)
 
 
